d7f90fb34c8e0a09ace0260c6ccd10fb26cb1e03
Mabast Hashm Jamel <mabasthashm1@gmail.com>
2018-08-22 20:28:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,53 +1,37 @@
-// Code your solution in this file!
-const drivers = [
-    { name: 'Bobby',   hometown: 'Pittsburgh',  revenue: 3000 },
-    { name: 'Sally',   hometown: 'New York',    revenue: 2000 },
-    { name: 'Sammy',   hometown: 'Pittsburgh',  revenue: 2500 },
-    { name: 'Annette', hometown: 'Los Angeles', revenue: 6000 },
-    { name: 'Bobby',   hometown: 'Tampa Bay',   revenue: 5000 }
-  ];
-  
- const logDriverNames = function(arr){
-   arr.forEach(function(el, i, arr){
-     console.log(el.name);
-   });
- }
- 
- 
-const logDriversByHometown = function(arr,location){
-  arr.forEach(function(el, i, arr){
-    if (el.hometown == location)
-    console.log (el.name);
+const logDriverNames = function(array)
+{
+  array.forEach(function(item)
+  {
+    console.log(item.name);
   });
 }
 
-const driversByRevenue = function(drivers)
-{
-   return drivers.slice().sort(function(d1,d2){
-     return d1.revenue - d2.revenue;
-   })
-}
+const logDriversByHometown = function (array, location) {
+  array.forEach(function (item) {
+    if (item.hometown === location) {
+      console.log(item.name);
+    }
+  });
+};
 
-const driversByName = function(drivers)
-{
-  return drivers.slice().sort(function(d1,d2)
-  {
-    return d1.name.localeCompare(d2.name)
-  })
-}
+const driversByRevenue = function (array) {
+  return array.slice().sort(function (first, second) {
+    return fisrt.revenue - second.revenue;
+  });
+};
 
- const totalRevenue = function (drivers) {
+const driversByName = function (drivers) {
+  return drivers.slice().sort(function (driverOne, driverTwo) {
+    return driverOne.name.localeCompare(driverTwo.name);
+  });
+};
+
+const totalRevenue = function (drivers) {
   return drivers.reduce(function (total, currentDriver) {
     return currentDriver.revenue + total;
   }, 0);
 };
 
-const averageRevenue = function(drivers)
-{
-  let total = 0;
-  drivers.forEach(function(driver){total+=driver.revenue});
-  
-  return total/drivers.length;
-}
-
-
+const averageRevenue = function (drivers) {
+  return totalRevenue(drivers) / drivers.length;
+};
